c57e7b88c20e79dcf3a73a9fdb1282a9411a8b2f
kolosy <kolosy@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2009-07-24 22:04:27 +0000

    matching branch for bistro 0910 release

---
+++ Integration/BistroIntegration/branches/Bistro-r0.9.1.0/Validation/ValidationTag.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bistro.Validation;
+
+namespace NDjango.BistroIntegration.Validation
+{
+    /// <summary>
+    /// Default implementation of the {% validate %} tag
+    /// </summary>
+    class ValidationTag: NDjango.Compatibility.SimpleTag
+    {
+        public ValidationTag() : base(false, "validate", -1) { }
+
+        /// <summary>
+        /// Processes the tag.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="parms">The parms.</param>
+        /// <returns></returns>
+        public override string ProcessTag(string content, object[] parms)
+        {
+            IValidator v = null;
+            foreach (string ns in parms)
+                v =
+                    v == null ?
+                    ValidationRepository.Instance.GetValidatorForNamespace(ns) :
+                    v.Merge(ValidationRepository.Instance.GetValidatorForNamespace(ns));
+
+            return
+                new StringBuilder()
+                    .AppendLine("\r\n<script type=\"text/javascript\">")
+                    .AppendLine("if (validation == undefined) var validation = new Array();")
+                    .AppendLine(JSEmitter.Instance.Emit(v, "validation[\"" + v.Name + "\"]"))
+                    .AppendLine("</script>")
+                    .ToString();
+        }
+    }
+}
