2582c5fb34dad868742f50f5a838778f1247ac7b
kirillskrygan@gmail.com <kirillskrygan@gmail.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2009-07-31 16:18:04 +0000

    Some comments. I hope all is correct.

--- NDjango/branches/NDjangoDesigner/NDjangoParser/Interfaces.cs
+++ NDjango/branches/NDjangoDesigner/NDjangoParser/Interfaces.cs
@@ -11,17 +11,59 @@ namespace NDjango.Interfaces
         List<INode> Parse(IEnumerable<string> template);
     }
 
+    /// <summary>
+    /// The type of the node. Depending on it, the node may provide different functionality
+    /// </summary>
     public enum NodeType
     {
+        /// <summary>
+        /// The whole tag.
+        /// <example>{% if somevalue %}</example>
+        /// </summary>
         Tag,
+        /// <summary>
+        /// The markers, which frame django tag. 
+        /// <example>{%, %}</example>
+        /// </summary>
         Marker,
+        /// <summary>
+        /// Django template tag.
+        /// <example> "with", "for", "ifequal"</example>
+        /// </summary>
         TagName,
+        /// <summary>
+        /// The keyword, that is necessary for some tags.
+        /// <example>"and", "as"</example>
+        /// </summary>
         Keyword,
+        /// <summary>
+        /// The variable of expression.
+        /// <example>In "User.DoB" expression "DoB" is a variable</example>
+        /// </summary>
         Variable,
+        /// <summary>
+        /// Expression, which may contain variables and filters
+        /// <example>User.DoB|date:"D d M Y"</example>
+        /// </summary>
         Expression,
+        /// <summary>
+        /// 
+        /// </summary>
         Reference,
+        /// <summary>
+        /// The filter or a group of filters. May contain parameters.
+        /// <example>first|length|default:"nothing"</example>
+        /// </summary>
         Filter,
+        /// <summary>
+        /// The name of the filter.
+        /// <example>"length", "first", "default"</example>
+        /// </summary>
         FilterName,
+        /// <summary>
+        /// Filter parameter.
+        /// <example>any valid value</example>
+        /// </summary>
         FilterParam
     };
 
@@ -33,6 +75,9 @@ namespace NDjango.Interfaces
         public const string NODELIST_IFTAG_IFFALSE = "if.false.children";
     }
 
+    /// <summary>
+    /// An error message consists of message, and severity level as well.
+    /// </summary>
     public struct Error
     {
         public Error(int severity, string message)
@@ -54,6 +99,9 @@ namespace NDjango.Interfaces
         string Text { get; }
         IEnumerable<string> Values { get; }
         Error ErrorMessage { get; }
+        /// <summary>
+        /// Indicates the quick info message.
+        /// </summary>
         string Info { get; }
         Dictionary<string, IEnumerable<INode>> Nodes { get; }
     }
