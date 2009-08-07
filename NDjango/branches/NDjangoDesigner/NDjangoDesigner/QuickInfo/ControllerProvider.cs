f56fd2def621befcbff9e8c12023df5e397be0cd
mfeingold@hill30.com <mfeingold@hill30.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2009-08-07 21:58:10 +0000

    Designer: QuickInfo diagnostics - some cleanup

--- NDjango/branches/NDjangoDesigner/NDjangoDesigner/QuickInfo/ControllerProvider.cs
+++ NDjango/branches/NDjangoDesigner/NDjangoDesigner/QuickInfo/ControllerProvider.cs
@@ -23,7 +23,7 @@ namespace NDjango.Designer.QuickInfo
         private IQuickInfoBrokerMapService brokerMapService { get; set; }
 
         [Import]
-        internal INodeProviderBroker parser { get; set; }
+        internal INodeProviderBroker nodeProviderBroker { get; set; }
 
         public IIntellisenseController TryCreateIntellisenseController(ITextView textView, IList<ITextBuffer> subjectBuffers, IEnvironment context)
         {
@@ -31,14 +31,14 @@ namespace NDjango.Designer.QuickInfo
             bool brokerCreated = false;
             foreach (ITextBuffer subjectBuffer in subjectBuffers)
             {
-                if (parser.IsNDjango(subjectBuffer))
+                if (nodeProviderBroker.IsNDjango(subjectBuffer))
                     brokerCreated |= (brokerMapService.GetBrokerForTextView(textView, subjectBuffer) != null);
             }
 
             // There may not be a broker for any of the subject buffers for this text view.  This can happen if there are no providers available.
             if (brokerCreated)
             {
-                return new Controller(parser, subjectBuffers, textView, brokerMapService);
+                return new Controller(nodeProviderBroker, subjectBuffers, textView, brokerMapService);
             }
 
             return null;
