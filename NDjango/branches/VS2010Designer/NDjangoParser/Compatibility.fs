648ce575f6f775a7fd2abc96ce309b5385711e36
mfeingold@hill30.com <mfeingold@hill30.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2009-08-13 16:27:59 +0000

    made a copy

---
+++ NDjango/branches/VS2010Designer/NDjangoParser/Compatibility.fs
@@ -0,0 +1,89 @@
+﻿(****************************************************************************
+ * 
+ *  NDjango Parser Copyright © 2009 Hill30 Inc
+ *
+ *  This file is part of the NDjango Parser.
+ *
+ *  The NDjango Parser is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The NDjango Parser is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with NDjango Parser.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ ***************************************************************************)
+
+#light
+
+namespace NDjango.Compatibility
+
+open NDjango.Lexer
+open NDjango.Interfaces
+open NDjango.OutputHandling
+open NDjango.Expressions
+open NDjango.ParserNodes
+
+/// Abstract tag implementation designed for consumption outside of F#. This class
+/// will handle interaction with the expression system and the parser, providing
+/// the abstract 'ProcessTag' method with the execution-time values of the supplied
+/// parameters, along with the fully resolved text of the nested value (if in nested mode).
+/// Concrete implementations are required to define a 0-parameter constructor, and
+/// pass in relevant values for 'nested' and 'name'. The value of 'name' must match
+/// the name the tag is registered under, but is only applicable when 'nested' is true.
+[<AbstractClass>]
+type public SimpleTag(nested:bool, name:string, num_params:int) =
+    /// Resolves all expressions in the list against the context
+    let resolve_all list (context: IContext) =
+        list |>
+        List.map (fun (e: FilterExpression) -> 
+                    match fst <| e.Resolve context true with
+                    | None -> null
+                    | Some v -> v) |>
+        List.to_array
+
+    /// Evaluates the contents of the nodelist against the given walker. This function
+    /// effectively parses the nested tags within the simple tag.
+    let read_walker manager walker nodelist =
+        let reader = 
+            new NDjango.ASTWalker.Reader (manager, {walker with parent=None; nodes=nodelist; context=walker.context})
+        reader.ReadToEnd()
+        
+    /// Tag implementation. This method will receive the fully-evaluated nested contents for nested tag
+    /// along with fully resolved values of the parameters supplied to the tag. Parameters in the template
+    /// source may follow standard parameter conventions, e.g. they can be variables or literals, with 
+    /// filters.
+    abstract member ProcessTag: context:IContext -> content:string -> parms:obj array -> string 
+    
+    member x.GetFromContext (context:IContext) (key:string) =
+        match context.tryfind key with
+        | None -> null
+        | Some v -> v
+    
+    interface ITag with
+        member x.Perform token provider tokens = 
+            let parms = 
+                token.Args |>
+                List.map (fun elem -> new FilterExpression(provider, Block token, elem))
+            
+            if not (parms.Length = num_params || num_params = -1) then
+                raise (SyntaxError(sprintf "%s expects %d parameters, but was given %d." name num_params (parms.Length)))
+            else
+                let nodelist, tokens =
+                    if nested then (provider :?> IParser).Parse (Some token) tokens ["end" + name]
+                    else [], tokens
+                    
+                ({new TagNode(provider, token)
+                    with
+                        override this.walk manager walker =
+                            let resolved_parms =  resolve_all parms walker.context
+                            if not nested then
+                                {walker with buffer = (x.ProcessTag walker.context "" resolved_parms)}
+                            else
+                                {walker with buffer = (x.ProcessTag walker.context (read_walker manager walker nodelist) resolved_parms)}
+                } :> INodeImpl), tokens
---
+++ NDjango/branches/VS2010Designer/NDjangoParser/Lexer.fs
@@ -0,0 +1,168 @@
+﻿(****************************************************************************
+ * 
+ *  NDjango Parser Copyright © 2009 Hill30 Inc
+ *
+ *  This file is part of the NDjango Parser.
+ *
+ *  The NDjango Parser is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The NDjango Parser is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with NDjango Parser.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ ***************************************************************************)
+
+
+namespace NDjango
+
+open System
+open System.IO
+open System.Collections
+open System.Collections.Generic
+
+open NDjango.OutputHandling
+
+module Lexer =
+
+    type TextToken(text:string, pos:int, line:int, linePos:int) =
+        member x.Text = text
+        member x.Position = pos
+        member x.Line = line
+        member x.LinePos = linePos
+        member x.Length = text.Length
+        override x.ToString() = sprintf " in token: \"%s\" at line %d pos %d " text line linePos
+
+    /// Exception raised when template syntax errors are encountered
+    and SyntaxErrorException (message: string, token: TextToken) =
+        inherit System.ApplicationException(message + token.ToString())
+        
+        member x.Token = token
+        member x.ErrorMessage = message  
+    
+    type BlockToken(text, pos, line, linePos) =
+        inherit TextToken(text, pos, line, linePos)
+        let verb, args = 
+            match smart_split (text.[Constants.BLOCK_TAG_START.Length..text.Length-Constants.BLOCK_TAG_END.Length-1].Trim()) with
+            | verb::args -> verb, args 
+            | _ -> raise (SyntaxError("Empty tag block"))
+        member x.Verb = verb 
+        member x.Args = args
+    
+    type ErrorToken(text, error:string, pos, line, linePos) =
+        inherit TextToken(text, pos, line, linePos)
+          
+        member x.ErrorMessage = error
+    
+    type VariableToken(text:string, pos, line, linePos) =
+        inherit TextToken(text, pos, line, linePos)
+        let expression = text.[Constants.VARIABLE_TAG_START.Length..text.Length-Constants.VARIABLE_TAG_END.Length-1].Trim()
+            
+        member this.Expression = 
+            if expression.Equals("") then
+                raise (SyntaxError("Empty variable block"))
+            expression 
+    
+    type CommentToken(text, pos, line, linePos) =
+        inherit TextToken(text, pos, line, linePos) 
+    
+    /// A lexer token produced through the tokenize function
+    type Token =
+        | Block of BlockToken
+        | Variable of VariableToken
+        | Comment of CommentToken
+        | Error of ErrorToken
+        | Text of TextToken
+        
+    let get_textToken = function
+    | Block b -> b :> TextToken
+    | Error e -> e :> TextToken
+    | Variable v -> v :> TextToken
+    | Comment c -> c :> TextToken
+    | Text t -> t
+        
+    /// <summary> generates sequence of tokens out of template TextReader </summary>
+    /// <remarks>the type implements the IEnumerable interface (sequence) of templates
+    /// It reads the template text from the text reader one buffer at a time and 
+    /// returns tokens in batches - a batch is a sequence of the tokens generated 
+    /// off the content of the buffer </remarks>
+    type private Tokenizer (template:TextReader) =
+        let mutable current: Token list = []
+        let mutable line = 0
+        let mutable pos = 0
+        let mutable linePos = 0
+        let mutable tail = ""
+        let buffer = Array.create 4096 ' '
+        
+        let create_token in_tag text = 
+            in_tag := not !in_tag
+            let currentPos = pos
+            let currentLine = line
+            let currentLinePos = linePos
+            Seq.iter 
+                (fun ch -> 
+                    pos <- pos + 1
+                    if ch = '\n' then 
+                        line <- line + 1 
+                        linePos <- 0
+                    else linePos <- linePos + 1
+                    ) 
+                text
+            if not !in_tag then
+                Text(new TextToken(text, currentPos, currentLine, currentLinePos))
+            else
+                try
+                    match text.[0..1] with
+                    | "{{" -> Variable (new VariableToken(text, currentPos, currentLine, currentLinePos))
+                    | "{%" -> Block (new BlockToken(text, currentPos, currentLine, currentLinePos))
+                    | "{#" -> Comment (new CommentToken(text, currentPos, currentLine, currentLinePos))
+                    | _ -> Text (new TextToken(text, currentPos, currentLine, currentLinePos))
+                with
+                | :? SyntaxError as ex -> 
+                    Error (new ErrorToken(text, ex.Message, currentPos, currentLine, currentLinePos))
+                | _ -> rethrow()
+        
+        interface IEnumerator<Token seq> with
+            member this.Current = Seq.of_list current
+        
+        interface IEnumerator with
+            member this.Current = Seq.of_list current :> obj
+            
+            member this.MoveNext() =
+                match tail with
+                | null -> 
+                    false
+                | _ -> 
+                    let count = template.ReadBlock(buffer, 0, buffer.Length)
+                    let strings = (Constants.tag_re.Split(tail + new String(buffer, 0, count)))
+                    let t, strings = 
+                        if (count > 0) then strings.[strings.Length-1], strings.[0..strings.Length - 2]
+                        else null, strings
+                    
+                    tail <- t
+                    let in_tag = ref true
+                    current <- strings |> List.of_array |> List.map (create_token in_tag)
+                    true
+                
+            member this.Reset() = failwith "Reset is not supported by Tokenizer"
+        
+        interface IEnumerable<Token seq> with
+            member this.GetEnumerator():IEnumerator<Token seq> =
+                 this :> IEnumerator<Token seq>
+
+        interface IEnumerable with
+            member this.GetEnumerator():IEnumerator =
+                this :> IEnumerator
+
+        interface IDisposable with
+            member this.Dispose() = ()
+
+    /// Produces a sequence of token objects based on the template text
+    let internal tokenize (template:TextReader) =
+        LazyList.of_seq <| Seq.fold (fun (s:Token seq) (item:Token seq) -> Seq.append s item) (seq []) (new Tokenizer(template))
