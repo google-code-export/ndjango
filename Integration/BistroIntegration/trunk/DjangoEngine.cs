aba6ff6f0bc181aa4ccec102fc1be0d13e18dd42
ABadylin@hill30.com <ABadylin@hill30.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2009-08-24 16:21:32 +0000

    removed filters dependency

--- Integration/BistroIntegration/trunk/DjangoEngine.cs
+++ Integration/BistroIntegration/trunk/DjangoEngine.cs
@@ -4,7 +4,6 @@ using Bistro.Controllers;
 using System.IO;
 using System.Web;
 using System;
-using NDjango.FiltersCS;
 using NDjango.Interfaces;
 using System.Configuration;
 using System.Reflection;
