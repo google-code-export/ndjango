3e230e7fde33b40246e5cb354d0020c939f4246c
kirillskrygan@gmail.com <kirillskrygan@gmail.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2009-08-27 11:35:28 +0000

    Designer tests refactoring

--- NDjango/branches/VS2010Designer/NDjangoUnitTests/Tests/Designer.cs
+++ NDjango/branches/VS2010Designer/NDjangoUnitTests/Tests/Designer.cs
@@ -9,108 +9,128 @@ namespace NDjango.UnitTests
 {
     public partial class Tests
     {
+        List<TestDescriptor> tests = new List<TestDescriptor>();
+
         //[Test, TestCaseSource("GetDesignerTests")]
         public void DesignerTests(TestDescriptor test)
         {
+           
             InternalFilterProcess(test);
+        }
+
+        private DesignerData[] Nodes(params DesignerData[] nodes) { return nodes; }
+
+        private string[] EmptyList
+        {
+            get
+            {
+                return new string[] { };
+            }
         }
 
         public IList<TestDescriptor> GetDesignerTests()
         {
-            IList<TestDescriptor> lst = new List<TestDescriptor>();
-            lst.Add(new TestDescriptor("if-tag-designer", "{% if foo %}yes{% else %}no{% endif %}", ContextObjects.p("foo", true), null
-                , new List<DesignerData>() 
-                {
-                    //position, length, values, severity, errorMessage
-                    new DesignerData(0, 0, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(0, 12, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(25, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(27, 11, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(29, 6, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(27, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(36, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(12, 3, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(15, 10, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(17, 5, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(15, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(23, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(2, 3, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(0, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(10, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(38, 0, new List<string>().ToArray(), -1, String.Empty)
-                }
-                , null));
-            lst.Add(new TestDescriptor("ifequal-tag-designer", "{% ifequal foo bar %}yes{% else %}no{% endifequal %}", ContextObjects.p("foo", true, "bar", true), null
-                , new List<DesignerData>() 
-                {
-                    //position, length, values, severity, errorMessage
-                    new DesignerData(0, 0, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(0, 21, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(34, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(36, 16, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(38, 11, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(36, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(50, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(21, 3, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(24, 10, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(26, 5, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(24, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(32, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(2, 8, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(0, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(19, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(52, 0, new List<string>().ToArray(), -1, String.Empty)
-                }
-                , null));
-            lst.Add(new TestDescriptor("add-filter-designer", "{{ value| add:\"2\" }}", null, null
-                , new List<DesignerData>() 
-                {
-                    //position, length, values, severity, errorMessage
-                    new DesignerData(0, 0, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(0, 20, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(0, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(18, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(20, 0, new List<string>().ToArray(), -1, String.Empty)
-                }
-                , null));
-            lst.Add(new TestDescriptor("fortag-designer", "{% for i in test %}{% ifchanged %}nothing changed{%else%}same {% endifchanged %}{{ forloop.counter }},{% endfor %}", null, null
-                ,new List<DesignerData>() 
-                {
-                    //position, length, values, severity, errorMessage
-                    new DesignerData(0, 0, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(0, 19, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(19, 0, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(19, 15, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(57, 5, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(62, 18, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(64, 13, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(62, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(78, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(34, 15, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(49, 8, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(51, 4, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(49, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(55, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(21, 10, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(19, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(32, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(80, 0, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(80, 21, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(80, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(99, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(101, 1, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(102, 12, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(104, 7, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(102, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(112, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(2, 4, TestDescriptor.standartValues.ToArray(), -1, String.Empty),
-                    new DesignerData(0, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(17, 2, new List<string>().ToArray(), -1, String.Empty),
-                    new DesignerData(114, 0, new List<string>().ToArray(), -1, String.Empty)
-                }
-                , null));
+            NewTest("if-tag-designer", "{% if foo %}yes{% else %}no{% endif %}" 
+                , Nodes 
+                (
+                    Node(0, 0, EmptyList),
+                    Node(0, 12, EmptyList),
+                    Node(25, 2, EmptyList),
+                    Node(27, 11, EmptyList),
+                    Node(27, 11, TestDescriptor.standardValues),
+                    Node(27, 2, EmptyList),
+                    Node(36, 2, EmptyList),
+                    Node(12, 3, EmptyList),
+                    Node(15, 10, EmptyList),
+                    Node(17, 5, TestDescriptor.standardValues),
+                    Node(15, 2, EmptyList),
+                    Node(23, 2, EmptyList),
+                    Node(2, 3, EmptyList),
+                    Node(0, 2, EmptyList),
+                    Node(10, 2, EmptyList),
+                    Node(38, 0, EmptyList)
+                ));
+            NewTest("ifequal-tag-designer", "{% ifequal foo bar %}yes{% else %}no{% endifequal %}"
+                , Nodes 
+                (
+                    Node(0, 0, EmptyList),
+                    Node(0, 21, EmptyList),
+                    Node(34, 2, EmptyList),
+                    Node(36, 16, EmptyList),
+                    Node(38, 11, TestDescriptor.standardValues),
+                    Node(36, 2, EmptyList),
+                    Node(50, 2, EmptyList),
+                    Node(21, 3, EmptyList),
+                    Node(24, 10, EmptyList),
+                    Node(26, 5, TestDescriptor.standardValues),
+                    Node(24, 2, EmptyList),
+                    Node(32, 2, EmptyList),
+                    Node(2, 8, TestDescriptor.standardValues),
+                    Node(0, 2, EmptyList),
+                    Node(19, 2, EmptyList),
+                    Node(52, 0, EmptyList)
+                ));
+            NewTest("add-filter-designer", "{{ value| add:\"2\" }}"
+                , Nodes 
+                (
+                    Node(0, 0, EmptyList),
+                    Node(0, 20, EmptyList),
+                    Node(0, 2, EmptyList),
+                    Node(18, 2, EmptyList),
+                    Node(20, 0, EmptyList)
+                ));
+            NewTest("fortag-designer", "{% for i in test %}{% ifchanged %}nothing changed{%else%}same {% endifchanged %}{{ forloop.counter }},{% endfor %}"
+                , Nodes 
+                (
+                    Node(0, 0, EmptyList),
+                    Node(0, 19, EmptyList),
+                    Node(19, 0, EmptyList),
+                    Node(19, 15, EmptyList),
+                    Node(57, 5, EmptyList),
+                    Node(62, 18, EmptyList),
+                    Node(64, 13, TestDescriptor.standardValues),
+                    Node(62, 2, EmptyList),
+                    Node(78, 2, EmptyList),
+                    Node(34, 15, EmptyList),
+                    Node(49, 8, EmptyList),
+                    Node(51, 4, TestDescriptor.standardValues),
+                    Node(49, 2, EmptyList),
+                    Node(55, 2, EmptyList),
+                    Node(21, 10, TestDescriptor.standardValues),
+                    Node(19, 2, EmptyList),
+                    Node(32, 2, EmptyList),
+                    Node(80, 0, EmptyList),
+                    Node(80, 21, EmptyList),
+                    Node(80, 2, EmptyList),
+                    Node(99, 2, EmptyList),
+                    Node(101, 1, EmptyList),
+                    Node(102, 12, EmptyList),
+                    Node(104, 7, TestDescriptor.standardValues),
+                    Node(102, 2, EmptyList),
+                    Node(112, 2, EmptyList),
+                    Node(2, 4, TestDescriptor.standardValues),
+                    Node(0, 2, EmptyList),
+                    Node(17, 2, EmptyList),
+                    Node(114, 0, EmptyList)
+                ));
 
-            return lst;
+            return tests;
         }
+
+        private DesignerData ErrorNode(int position, int length, string[] values, int errorSeverity, string errorMessage)
+        {
+            return new DesignerData(position, length, values, errorSeverity, errorMessage);
+        }
+
+        private DesignerData Node(int position, int length, string[] values)
+        {
+            return ErrorNode(position, length, values, -1, String.Empty);
+        }
+
+        private void NewTest(string name, string template, DesignerData[] nodeList)
+        {
+            tests.Add(new TestDescriptor(name, template, nodeList.ToList<DesignerData>()));
+        }
+
     }
 }
