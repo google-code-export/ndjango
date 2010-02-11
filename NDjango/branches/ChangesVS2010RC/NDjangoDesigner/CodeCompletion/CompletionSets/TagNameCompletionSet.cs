65cdb78c20720dbfa582c4323929cfc840c19a16
denis@elkhov.net <denis@elkhov.net@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2010-02-11 18:23:58 +0000

    Some fixes for VS 2010 RC

---
+++ NDjango/branches/ChangesVS2010RC/NDjangoDesigner/CodeCompletion/CompletionSets/TagNameCompletionSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NDjango.Designer.Parsing;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Language.Intellisense;
+
+namespace NDjango.Designer.CodeCompletion.CompletionSets
+{
+    /// <summary>
+    /// Represents a list of completions for available tag names
+    /// </summary>
+    class TagNameCompletionSet : AbstractCompletionSet
+    {
+        internal TagNameCompletionSet(DesignerNode node, SnapshotPoint point)
+            : base(node, point)
+        { }
+
+        private IEnumerable<Completion> BuildCompletions(IEnumerable<string> values)
+        {
+            return BuildCompletions(values, "", "");
+        }
+
+        protected override IEnumerable<Completion> BuildNodeCompletions()
+        {
+            return BuildCompletions(Node.ParsingContext.Tags); 
+        }
+
+        protected override IEnumerable<Completion> BuildNodeCompletionBuilders()
+        {
+            return BuildCompletions(Node.ParsingContext.TagClosures);
+        }
+    }
+}
