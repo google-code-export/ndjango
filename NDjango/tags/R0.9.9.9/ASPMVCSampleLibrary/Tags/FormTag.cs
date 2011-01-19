81fe7688fa7be1de3fdf3e596b4c6bc9f165284b
sasha_vlasova@mail.ru <sasha_vlasova@mail.ru@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2011-01-19 13:01:10 +0000

    tag R.0.9.9.9:Smart If,NuGet package,only vsix - no msi

---
+++ NDjango/tags/R0.9.9.9/ASPMVCSampleLibrary/Tags/FormTag.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NDjango.Interfaces;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.IO;
+
+namespace ASPMVCSampleLibrary
+{
+    [NDjango.ParserNodes.Description("Django wrapper around HtmlHelper.BeginForm")]
+    [Name("form")]
+    public class FormTag : NDjango.Compatibility.SimpleTag
+    {
+        public FormTag()
+            : base(true, 0)
+        { }
+
+        public override string ProcessTag(NDjango.Interfaces.IContext context, string content, object[] parms)
+        {
+            var htmlHelperOption = context.tryfind("Html");
+            if (htmlHelperOption == null)
+                return "";
+
+            var htmlHelper = (HtmlHelper)htmlHelperOption.Value;
+
+            var string_writer = new StringWriter();
+            var writer = htmlHelper.ViewContext.Writer;
+            try
+            {
+                htmlHelper.ViewContext.Writer = string_writer;
+                var form = htmlHelper.BeginForm();
+                string_writer.Flush();
+                var start = string_writer.ToString();
+
+                string_writer = new StringWriter();
+                htmlHelper.ViewContext.Writer = string_writer;
+                form.EndForm();
+                string_writer.Flush();
+                var end = string_writer.ToString();
+
+                return start + content + end;
+            }
+            finally
+            {
+                htmlHelper.ViewContext.Writer = writer;
+            }
+
+        }
+    }
+}
